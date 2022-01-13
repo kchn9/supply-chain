a3f9f43d6bd885a5bc6992ebe272a9ef73c63c3a
Mateusz <44598863+kchn9@users.noreply.github.com>
2022-01-13 12:32:21 +0100

    Add createItem() and manager state

--- SupplyChain.sol
+++ SupplyChain.sol
@@ -2,5 +2,28 @@
 pragma solidity ^0.8.0;
 
 contract ItemManager {
+    enum SupplyChainSteps {Created, Paid, Delivery } // to express state of order
 
+    struct SupplyItem {
+        ItemManager.SupplyChainSteps _step;
+        string _identifier;
+        uint _priceInWei;
+    }
+    mapping (uint => SupplyItem) public items;
+    uint index;
+
+    event SupplyChainStepChanged(uint _itemIndex, uint _step);
+
+    /**
+     * @dev Adds item to register
+     * @param _identifier Name of item
+     * @param _priceInWei Wei price of item
+     */
+    function createItem(string memory _identifier, uint _priceInWei) public {
+        items[index]._step = SupplyChainSteps.Created;
+        items[index]._identifier = _identifier;
+        items[index]._priceInWei = _priceInWei;
+        emit SupplyChainStepChanged(index, uint(items[index]._step));
+        index++;
+    }
 }
