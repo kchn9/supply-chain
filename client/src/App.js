c68ca62a35dcd6ed8746cd8b4c2cfe10c68f2469
kchn9 <mat.kuchnia@gmail.com>
2022-04-05 20:55:45 -0500

    Prettify

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,28 @@
 import React, { useState, useEffect, useCallback } from "react";
 import ItemTable from "./components/ItemTable/ItemTable";
 import AddItemForm from "./components/AddItemForm/AddItemForm";
-import { Button, Grid, Typography, Stack, Divider, Box, Container } from "@mui/material/";
-import Jazzicon, { jsNumberForAddress } from 'react-jazzicon';
+import {
+  Button,
+  Grid,
+  Typography,
+  Stack,
+  Divider,
+  Box,
+  Container,
+} from "@mui/material/";
+import Jazzicon, { jsNumberForAddress } from "react-jazzicon";
 
 /**
  * Import web3 & contracts
  */
 import ItemManagerContract from "./contracts/ItemManager.json";
-import ItemContract from './contracts/Item.json';
+import ItemContract from "./contracts/Item.json";
 
 import getWeb3 from "./getWeb3";
 
 const App = () => {
-  const [itemCost, setItemCost] = useState('');
-  const [itemIdentifier, setItemIdentifier] = useState('');
+  const [itemCost, setItemCost] = useState("");
+  const [itemIdentifier, setItemIdentifier] = useState("");
 
   const [web3, setWeb3] = useState(null);
   useEffect(() => {
@@ -25,36 +33,42 @@ const App = () => {
       } catch (error) {
         console.error(error);
       }
-    }
+    };
     initializeWeb3();
-  }, []) // init web3
+  }, []); // init web3
 
-  const [currentAccount, setCurrentAccount] = useState('');
+  const [currentAccount, setCurrentAccount] = useState("");
   useEffect(() => {
     const fetchAccounts = async () => {
       try {
-        const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+        const accounts = await window.ethereum.request({
+          method: "eth_accounts",
+        });
         setCurrentAccount(web3.utils.toChecksumAddress(accounts[0]));
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
-    }
+    };
     if (web3) fetchAccounts();
-  }, [web3]) // init accounts
+  }, [web3]); // init accounts
 
   useEffect(() => {
     if (web3) {
       if (window.ethereum) {
-        window.ethereum.on('accountsChanged', (accounts) => {
-          setCurrentAccount(web3.utils.toChecksumAddress(accounts[0]))
-        })
+        window.ethereum.on("accountsChanged", (accounts) => {
+          setCurrentAccount(web3.utils.toChecksumAddress(accounts[0]));
+        });
       } else if (window.web3) {
-        window.web3.currentProvider.on('accountsChanged', (accounts) => setCurrentAccount(web3.utils.toChecksumAddress(accounts[0])))
+        window.web3.currentProvider.on("accountsChanged", (accounts) =>
+          setCurrentAccount(web3.utils.toChecksumAddress(accounts[0]))
+        );
       } else {
-        console.log('Fallback to localhost detected - subscription to account change is being ignored now.')
+        console.log(
+          "Fallback to localhost detected - subscription to account change is being ignored now."
+        );
       }
     }
-  }, [web3])
+  }, [web3]);
 
   const [itemManager, setItemManager] = useState(null);
   useEffect(() => {
@@ -62,24 +76,25 @@ const App = () => {
       try {
         const instance = new web3.eth.Contract(
           ItemManagerContract.abi,
-          ItemManagerContract.networks[await web3.eth.net.getId()] && ItemManagerContract.networks[await web3.eth.net.getId()].address,
+          ItemManagerContract.networks[await web3.eth.net.getId()] &&
+            ItemManagerContract.networks[await web3.eth.net.getId()].address
         );
         setItemManager(instance);
       } catch (error) {
         console.error(error);
       }
-    }
+    };
     if (web3) createItemManager();
   }, [web3]); // init main contract
 
-  const [owner, setOwner] = useState('0x00');
+  const [owner, setOwner] = useState("0x00");
   useEffect(() => {
     const getOwner = async () => {
       const owner = await itemManager.methods.owner().call();
       setOwner(owner);
-    }
+    };
     if (web3 && itemManager) getOwner();
-  }, [web3, itemManager])
+  }, [web3, itemManager]);
 
   const [items, setItems] = useState([]);
   const fetchPreviousItems = useCallback(async () => {
@@ -88,39 +103,49 @@ const App = () => {
       const prevItems = [];
       for (let i = 0; i < index; i++) {
         const item = await itemManager.methods.items(i).call();
-        const itemInstance = new web3.eth.Contract(ItemContract.abi, item._item); // for fetching price
+        const itemInstance = new web3.eth.Contract(
+          ItemContract.abi,
+          item._item
+        ); // for fetching price
         prevItems.push({
           index: i,
           identifier: item._identifier,
           price: await itemInstance.methods.priceInWei().call(),
           step: item._step,
-          address: item._item
+          address: item._item,
         });
       }
       setItems(prevItems);
     } catch (error) {
       console.error(error);
     }
-  }, [web3, itemManager])
+  }, [web3, itemManager]);
   useEffect(() => {
     if (itemManager) fetchPreviousItems();
-  }, [itemManager, fetchPreviousItems]) // fetch previous items
+  }, [itemManager, fetchPreviousItems]); // fetch previous items
   useEffect(() => {
     if (itemManager) {
-      itemManager.events.ItemCreation().on("data", event => {
-        setItems(prev => [...prev, {
-          index: event.returnValues._itemIndex,
-          identifier: event.returnValues._identifier,
-          price: event.returnValues._price,
-          step: event.returnValues._step,
-          address: event.returnValues._itemAddress
-        }]);
-      })
-      itemManager.events.ItemPaid().on("data", event => {
-        console.log(`Item of ID: ${event.returnValues._itemIndex} at address ${event.returnValues._itemAddress} has been paid for.`)
+      itemManager.events.ItemCreation().on("data", (event) => {
+        setItems((prev) => [
+          ...prev,
+          {
+            index: event.returnValues._itemIndex,
+            identifier: event.returnValues._identifier,
+            price: event.returnValues._price,
+            step: event.returnValues._step,
+            address: event.returnValues._itemAddress,
+          },
+        ]);
       });
-      itemManager.events.ItemDelivery().on("data", event => {
-        console.log(`Item of ID: ${event.returnValues._itemIndex} at address ${event.returnValues._itemAddress} is being delivered now.`)
+      itemManager.events.ItemPaid().on("data", (event) => {
+        console.log(
+          `Item of ID: ${event.returnValues._itemIndex} at address ${event.returnValues._itemAddress} has been paid for.`
+        );
+      });
+      itemManager.events.ItemDelivery().on("data", (event) => {
+        console.log(
+          `Item of ID: ${event.returnValues._itemIndex} at address ${event.returnValues._itemAddress} is being delivered now.`
+        );
       });
     }
   }, [itemManager]); // subscribe for ItemCreation event - event trigger
@@ -132,32 +157,34 @@ const App = () => {
       try {
         const instance = new web3.eth.Contract(
           ItemContract.abi,
-          selectedItemAddress,
-        )
+          selectedItemAddress
+        );
         setItem(instance);
       } catch (error) {
         console.error(error);
       }
-    } 
+    };
     if (web3 && selectedItemAddress) changeSelectedItem();
-  }, [web3, selectedItemAddress]) // change Item contract on selection
+  }, [web3, selectedItemAddress]); // change Item contract on selection
 
   const [selectedItemIndex, setSelectedItemIndex] = useState(null);
   useEffect(() => {
     const getItemIndex = async () => {
       const index = await item.methods.index().call();
       setSelectedItemIndex(index);
-    }
+    };
     if (item) getItemIndex();
-  }, [item])
+  }, [item]);
 
   const handleItemCreationSubmit = async (event) => {
     event.preventDefault();
     const from = currentAccount;
     try {
-        await itemManager.methods.createItem(itemIdentifier, itemCost).send({ from: from });
+      await itemManager.methods
+        .createItem(itemIdentifier, itemCost)
+        .send({ from: from });
     } catch (e) {
-        console.error(e);
+      console.error(e);
     }
   };
 
@@ -169,102 +196,164 @@ const App = () => {
         from: from,
         to: selectedItemAddress,
         value: value,
-      })
+      });
     } catch (error) {
       console.error(error);
-    }
-    finally {
+    } finally {
       fetchPreviousItems();
     }
-  }
+  };
 
   const handleItemDelivery = async () => {
     const from = currentAccount;
     try {
-      await itemManager.methods.triggerDelivery(selectedItemIndex).send({ from: from });
+      await itemManager.methods
+        .triggerDelivery(selectedItemIndex)
+        .send({ from: from });
     } catch (error) {
       console.error(error);
-    }
-    finally {
+    } finally {
       fetchPreviousItems();
     }
-  }
+  };
 
   const handleItemChange = (event) => {
-    setSelectedItemAddress(event.target.value)
-  }
+    setSelectedItemAddress(event.target.value);
+  };
 
   return (
-    <Grid
-      container
-      spacing={4}
-      justifyContent="center"
-      padding={"0 32px"}
-
-    >
+    <Grid container spacing={4} justifyContent="center" padding={"0 32px"}>
       <Grid item xs={12}>
-        <Typography variant="h2" component="h1" paddingTop="48px" align="center">
+        <Typography
+          variant="h2"
+          component="h1"
+          paddingTop="48px"
+          align="center"
+        >
           Supply Chain Client
         </Typography>
       </Grid>
       <Grid item xs={12}>
-      {
-      web3
-        ? (
-          currentAccount !== "" ? 
-            (
-              <Stack direction={"row"} justifyContent={"center"} alignItems={"center"}>
-                <Typography align="center" variant="body1" paddingLeft={1} paddingRight={1}>Connected succefully to address {currentAccount}</Typography>
-                <Jazzicon diameter={30} seed={jsNumberForAddress(currentAccount)} />
-              </Stack>
-            ) 
-            : <Typography align="center" variant="body1">Web3 detected - make sure to connect your account</Typography> 
+        {web3 ? (
+          currentAccount !== "" ? (
+            <Stack
+              direction={"row"}
+              justifyContent={"center"}
+              alignItems={"center"}
+            >
+              <Typography
+                align="center"
+                variant="body1"
+                paddingLeft={1}
+                paddingRight={1}
+              >
+                Connected succefully to address {currentAccount}
+              </Typography>
+              <Jazzicon
+                diameter={30}
+                seed={jsNumberForAddress(currentAccount)}
+              />
+            </Stack>
+          ) : (
+            <Typography align="center" variant="body1">
+              Web3 detected - make sure to connect your account
+            </Typography>
           )
-        : (<Grid item xs={12}> <Typography align="center" variant="body1">Loading Web3, accounts, and contract... Please make sure your gateway is connected.</Typography> </Grid>)
-      }
+        ) : (
+          <Grid item xs={12}>
+            {" "}
+            <Typography align="center" variant="body1">
+              Loading Web3, accounts, and contract... Please make sure your
+              gateway is connected.
+            </Typography>{" "}
+          </Grid>
+        )}
       </Grid>
       <Grid item>
-        <Stack boxShadow={"#5f5f5f50 12px 12px 54px"} padding={2} marginBottom={8}>
+        <Stack
+          boxShadow={"#5f5f5f50 12px 12px 54px"}
+          padding={2}
+          marginBottom={8}
+        >
           <Stack direction={"row"} marginBottom={2} justifyContent="center">
-          {owner === currentAccount &&
-            <Container>
-              <Typography variant="h5" component={"h2"}>Add item:</Typography>
-              <AddItemForm
-                handleItemCreationSubmit={handleItemCreationSubmit}
-                setItemCost={setItemCost}
-                itemCost={itemCost}
-                setItemIdentifier={setItemIdentifier}
-                itemIdentifier={itemIdentifier}
-              />
-            </Container>
-          }
-            <Container>
-            {item ? (
-              <>
-                <Typography variant="h5" component={"h2"}>Item actions:</Typography>
-                <Stack spacing={4} maxWidth="60%" minHeight={"100px"} height={"calc(100% - 64px)"} justifyContent="center">
-                  <Button variant="contained" onClick={() => {handleItemPayment()}}>
-                    Pay
-                  </Button>
-                  {owner === currentAccount && <Button variant="contained" fullWidth onClick={() => {handleItemDelivery()}}>Delivery</Button>}
-                </Stack>
-              </>
-                ) : <Typography align="center" variant="body1">Select item to perform any action.</Typography>
-            }
+            {owner === currentAccount && (
+              <Container>
+                <Typography variant="h5" component={"h2"}>
+                  Add item:
+                </Typography>
+                <AddItemForm
+                  handleItemCreationSubmit={handleItemCreationSubmit}
+                  setItemCost={setItemCost}
+                  itemCost={itemCost}
+                  setItemIdentifier={setItemIdentifier}
+                  itemIdentifier={itemIdentifier}
+                />
+              </Container>
+            )}
+            <Container >
+              {item ? (
+                <>
+                  <Typography variant="h5" component={"h2"}>
+                    Item actions:
+                  </Typography>
+                  <Stack
+                    spacing={4}
+                    minHeight={"100px"}
+                    height={"calc(100% - 64px)"}
+                    justifyContent="center"
+                  >
+                    <Button
+                      variant="contained"
+                      onClick={() => {
+                        handleItemPayment();
+                      }}
+                      fullWidth
+                    >
+                      Order & Pay
+                    </Button>
+                    {owner === currentAccount && (
+                      <Button
+                        variant="contained"
+                        fullWidth
+                        onClick={() => {
+                          handleItemDelivery();
+                        }}
+                      >
+                        Run Delivery
+                      </Button>
+                    )}
+                  </Stack>
+                </>
+              ) : (
+                <Typography align="center" variant="body1">
+                  Select item to perform any action.
+                </Typography>
+              )}
             </Container>
           </Stack>
-          <Divider variant="middle"/>
-          <Box >
-            {items.length ? <Typography variant="h4" component={"h2"} align="center" marginTop={2} gutterBottom>Items:</Typography> : <Typography variant="body1" color={"gray"} marginBottom={2}>There is nothing to show.</Typography>}
-            <ItemTable
-              items={items}
-              handleItemChange={handleItemChange}
-            />
+          <Divider variant="middle" />
+          <Box>
+            {items.length ? (
+              <Typography
+                variant="h4"
+                component={"h2"}
+                align="center"
+                marginTop={2}
+                gutterBottom
+              >
+                Items:
+              </Typography>
+            ) : (
+              <Typography variant="body1" color={"gray"} marginBottom={2}>
+                There is nothing to show.
+              </Typography>
+            )}
+            <ItemTable items={items} handleItemChange={handleItemChange} />
           </Box>
         </Stack>
       </Grid>
     </Grid>
   );
-}
+};
 
 export default App;
